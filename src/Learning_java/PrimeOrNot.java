f4559147a52d256d8ce3fc27ef428fc2244e7575
Gillivalavan G <gillivalavang.cse@gmail.com>
2026-06-05 13:42:12 +0530

    create prime class

---
+++ src/Learning_java/PrimeOrNot.java
@@ -0,0 +1,19 @@
+package Learning_java;
+import java.util.*;
+public class PrimeOrNot {
+    public static void main(String [] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int flag = 0;
+    for(int i = 2; i < Math.sqrt(n); i++)
+    {
+        if(n % i == 0)
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if(flag == 0) System.out.print("Prime");
+    else System.out.print("Not a prime");
+  }
+}
