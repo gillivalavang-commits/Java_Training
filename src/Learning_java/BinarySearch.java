aacfa40b9823caf1050723758d4fa6700d5ad471
Gillivalavan G <gillivalavang.cse@gmail.com>
2026-06-06 22:19:24 +0530

    create binary search

--- src/Learning_java/BinarySearch.java
+++ src/Learning_java/BinarySearch.java
@@ -3,33 +3,39 @@ import java.util.*;
 public class BinarySearch {
     public static void main(String[] args)
     {
+
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
         int n = sc.nextInt();
+
         int[] arr = new int[n];
         for(int i = 0; i < n; i++)
         {
             arr[i] = sc.nextInt();
         }
+
         System.out.print("Enter target: ");
         int target = sc.nextInt();
+
         int start = 0;
         int end = arr.length - 1;
-        int mid = start + (end - start)/2;
         int flag = 0;
 
-        while(start < end)
+        while(start <= end)
         {
-            if(mid == target)
+            int mid = (start  + end)/2;
+
+            if(arr[mid] == target)
             {
                 flag = 1;
                 break;
             }
-            else if(mid < target)
+            else if(arr[mid] < target)
             {
-                mid += 1;
+                start = mid + 1;
             }
             else {
-                mid -= 1;
+                end = mid - 1;
             }
         }
         if(flag == 1)
